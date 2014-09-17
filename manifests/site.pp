aebb1baa0591b6ce01b155eae9868b171a4bdfa6
Onur SALK <salk.onur@gmail.com>
2014-09-17 18:42:00 +0300

    solr role added

--- manifests/site.pp
+++ manifests/site.pp
@@ -11,35 +11,5 @@ node 'server-1.wekanban.com' {
 
 node 'server-2.wekanban.com' {
   include stages
-  include yum
-  include wget
-  Class['yum'] -> Class['wget']
-  account {
-    'solr':
-      home_dir => '/home/solr',
-      groups   => [ 'wheel' ],
-      password => '$6$0hA2AT5I$KPHiddyTKO1y75Cl.e.FNCvBQ3oxjJlY5JBNaHWHjTnC5SyKCBf9m2Ot18.LASqx/vzz.gW9IX4Z9gDxhU3Pl1',
-      comment  => 'Solr user',
-  }
-  class {'epel':
-    stage => 'pre',
-  }
-  class { 'java':
-    distribution => 'jre',
-  }
-  service { 'iptables' :
-    ensure => 'stopped',
-    enable => 'false',
-  }
-  exec { 'solr-install':
-    cwd => '/opt',
-    command => '/usr/bin/wget http://apache.bilkent.edu.tr/lucene/solr/4.10.0/solr-4.10.0.tgz && /bin/tar xvzf solr-4.10.0.tgz && rm -rf solr-4.10.0.tgz && mv solr-4.10.0 solr && cd solr && mv example core ',
-    creates => '/opt/solr',
-    timeout => 0,
-  }
-  replace_matching_line { 'rootlogger':
-    file => '/opt/solr/core/resources/log4j.properties',
-    match => '=INFO',
-    replace => '=WARN',
-  }
+  include role::solr
 }
