694725426653f0c2f943bcde4608c6c846693a38
Onur SALK <salk.onur@gmail.com>
2014-09-17 18:44:55 +0300

    solr role added

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,11 @@
 import 'stages.pp'
 
-node 'server-1.wekanban.com' {
+node 'server-1.wekanban.com' {i
+  include stages
   include yum
   include role::general
   #include role::www:dev
 }
-
-
-
 
 node 'server-2.wekanban.com' {
   include stages
