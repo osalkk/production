0d53b15de2b121a3c94ff51bc6a47657c9ddbac8
Onur SALK <salk.onur@gmail.com>
2014-09-17 18:04:09 +0300

    rootlogger

--- modules/replace_matching_line/manifests/init.pp
+++ modules/replace_matching_line/manifests/init.pp
@@ -1,10 +1,8 @@
-class replace_matching_line {
 define replace_matching_line($file,$match,$replace) {
     exec { "/usr/bin/ruby -i -p -e 'sub(%r{$match}, \"$replace\")' ${file}":
     onlyif => "/bin/grep -E '${match}' ${file}",
     }
   }
-}
 
 replace_matching_line { 'rootlogger':
     file => '/opt/solr/core/log4j.properties',
