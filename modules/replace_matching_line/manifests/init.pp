a36df822fb1a802d7de0e739bd3e86b657bafa8c
Onur SALK <salk.onur@gmail.com>
2014-09-17 18:01:04 +0300

    rootlogger

---
+++ modules/replace_matching_line/manifests/init.pp
@@ -0,0 +1,7 @@
+class replace_matching_line {
+define replace_matching_line($file,$match,$replace) {
+    exec { "/usr/bin/ruby -i -p -e 'sub(%r{$match}, \"$replace\")' ${file}":
+    onlyif => "/bin/grep -E '${match}' ${file}",
+    }
+  }
+}
